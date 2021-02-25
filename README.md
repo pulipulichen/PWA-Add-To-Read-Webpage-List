a08d1cfc8e66b231531c002ef5c6c4df3a135b26
Pulipuli Chen <blog@pulipuli.info>
2021-02-25 14:30:35 +0800

    0225 做到一半，要怎麼成為share target呢

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PWA-Add-To-Read-Webpage-List
 A PWA for sharing title and URL to AppSheet
+
+https://pulipulichen.github.io/PWA-Add-To-Read-Webpage-List/
---
+++ script.js
@@ -0,0 +1,27 @@
+/* global fetch */
+
+function postData(url, data) {
+  // Default options are marked with *
+  return fetch(url, {
+    body: JSON.stringify(data), // must match 'Content-Type' header
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    //credentials: 'same-origin', // include, same-origin, *omit
+    headers: {
+      'Content-Type': 'text/plain;charset=utf-8',
+    },
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    //mode: 'cors', // no-cors, cors, *same-origin
+    //redirect: 'follow', // manual, *follow, error
+    //referrer: 'no-referrer', // *client, no-referrer
+  })
+  //.then(response => response.json()) // 輸出成 json
+}
+
+let url = 'https://script.google.com/macros/s/AKfycbzDb9jHAK9FyoBpEPTZVHLLyeJy_uFhItwStp-kugjbeQInM1CKalX0/exec'
+
+/*
+postData(url, {
+  title: 'test',
+  url: 'https://blog.pulipuli.info'
+})
+ */
