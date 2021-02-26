63d7e4eb84696d072c7f046218359f92fb6226e3
Pulipuli Chen <blog@pulipuli.info>
2021-02-26 17:13:45 +0800

    0226 可以嗎可以嗎？

--- target_files/common.js
+++ target_files/common.js
@@ -4,13 +4,13 @@
  * Installability requires a service worker with a fetch event handler, and
  * if the page isn't served over HTTPS, the service worker won't load.
  */
-if (window.location.protocol === 'http:') {
-  const requireHTTPS = document.getElementById('requireHTTPS');
-  const link = requireHTTPS.querySelector('a');
-  link.href = window.location.href.replace('http://', 'https://');
-  requireHTTPS.classList.remove('hidden');
-  window.location.href = 'https:' + window.location.href.substring(5);
-}
+//if (window.location.protocol === 'http:') {
+//  const requireHTTPS = document.getElementById('requireHTTPS');
+//  const link = requireHTTPS.querySelector('a');
+//  link.href = window.location.href.replace('http://', 'https://');
+//  requireHTTPS.classList.remove('hidden');
+//  window.location.href = 'https:' + window.location.href.substring(5);
+//}
 
 /* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
