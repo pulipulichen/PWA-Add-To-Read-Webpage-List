8855583cff5d547443ff72142477433768e2a7b7
Pulipuli Chen <blog@pulipuli.info>
2021-02-26 23:22:18 +0800

    0226 這樣子作為中繼站的PWA應該是可以了

--- script.js
+++ script.js
@@ -49,10 +49,63 @@ window.addEventListener('DOMContentLoaded', async () => {
       text: parsedUrl.searchParams.get('text'),
       url: parsedUrl.searchParams.get('url')
     })
+    console.log('data is added. close window.')
     window.close()
   }
   else {
-    document.getElementById('message').innerText = 'Please install PWA'
+    //document.getElementById('message').innerText = 'Please install PWA'
+    showInstallInformation()
+    setupBookmarklet()
   }
   
 });
+
+
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+let buttonInstall = document.getElementById('buttonInstall')
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  //showInstallPromotion();
+  buttonInstall.style.display = 'inline'
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
+
+
+buttonInstall.addEventListener('click', async () => {
+  // Hide the app provided install promotion
+  //hideInstallPromotion();
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  const {outcome} = await deferredPrompt.userChoice;
+  // Optionally, send analytics event with outcome of user choice
+  console.log(`User response to the install prompt: ${outcome}`);
+  // We've used the prompt, and can't use it again, throw it away
+  deferredPrompt = null;
+});
+
+
+let setupBookmarklet = function () {
+  let link = document.getElementById('bookmarklet')
+  
+  link.addEventListener('click', e => {
+    e.preventDefault()
+    e.stopPropagation()
+    
+    window.alert('Drag this link to your bookmark')
+  })
+  
+  link.href = `javascript: window.open("${location.href}?title=" + document.title + "&url=" + location.href, '_blank', "scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=100,height=100")`
+}
+
+let showInstallInformation = function () {
+  document.getElementById('information').style.display = 'block'
+  document.getElementById('loadingLayer').style.display = 'none'
+}
--- service-worker-loader.js
+++ service-worker-loader.js
@@ -1,13 +1,6 @@
 let enableServiceWorker = ('serviceWorker' in navigator)
-// enableServiceWorker = false
+//enableServiceWorker = false
 if (enableServiceWorker) {
-  navigator.serviceWorker.register('./service-worker.js')
-    .then(function (registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function (err) {
-
-      // registration failed
-      console.log('ServiceWorker registration failed: ', err);
-    });
+  console.log('👍', 'navigator.serviceWorker is supported');
+  navigator.serviceWorker.register('./service-worker.js');
 }
